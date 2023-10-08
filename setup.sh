493b4c1a5fb4840af084e2525adde5ee1f41703d
Tokito-Kun <93600306+Tokito-Kun@users.noreply.github.com>
2023-10-08 21:17:40 +0530

    cirrus: Update script with sleep

--- setup.sh
+++ setup.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 
-# install requests
-sudo pacman -Sy --noconfirm --needed python python-requests
+# Install requests
+sudo pacman -Sy --noconfirm --needed python-requests
 
 # Clone kernel
 git clone --single-branch --depth=1 https://github.com/Tokito-Kun/nexus_kernel_xiaomi_sm8250.git -b rebase-new kernel
-
-# Build
-cd kernel
-bash build.sh null alioth lto
-bash build.sh null munch lto
-bash build.sh null lmi lto
-bash build.sh null apollo lto
