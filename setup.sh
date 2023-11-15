eeeda8143ee9ca5b2a90f667528a60855fa670db
Tokito <93600306+Tokito-Kun@users.noreply.github.com>
2023-11-15 10:52:49 +0530

    Cirrus : Update branch `pelt-2`

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@
 sudo pacman -Sy --noconfirm --needed python-requests
 
 # Clone kernel
-git clone --single-branch --depth=1 https://github.com/Tokito-Kun/nexus_kernel_xiaomi_sm8250.git -b picel-sched kernel
+git clone --single-branch --depth=1 https://github.com/Tokito-Kun/nexus_kernel_xiaomi_sm8250.git -b pelt-2 kernel
