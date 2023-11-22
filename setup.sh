effd2d1f99ac3c0f598cef0e9272bf8176e4eda9
Tokito-Kun <93600306+Tokito-Kun@users.noreply.github.com>
2023-11-22 17:44:30 +0530

    cirrus:`update`

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+# Setup Timezone
+ln -fs /usr/share/zoneinfo/Asia/Kolkata /etc/localtime && date
+
 # Install requests
-sudo pacman -Sy --noconfirm --needed python-requests
+sudo pacman -Sy --noconfirm --needed --noprogressbar python-requests
 
 # Clone kernel
 git clone --depth=1 --quiet https://github.com/Tokito-Kun/nexus_kernel_xiaomi_sm8250.git -b pelt-2 kernel
