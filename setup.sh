b52edb6ab8ba8c1f0fdb6c48838919b1fee7f5f3
Yukimitsu-Tokito <93600306+Yukimitsu-Tokito@users.noreply.github.com>
2024-03-13 21:50:42 +0530

    cirrus: Update Branch
     * sched-4 Branch

--- setup.sh
+++ setup.sh
@@ -7,4 +7,4 @@ ln -fs /usr/share/zoneinfo/Asia/Kolkata /etc/localtime && date
 sudo pacman -Sy --noconfirm --needed --noprogressbar python-requests
 
 # Clone kernel
-git clone --depth=1 --quiet https://github.com/Tokito-Kun/nexus_kernel_xiaomi_sm8250.git -b pelt-2 kernel
+git clone --depth=1 https://github.com/Tokito-Kun/nexus_kernel_xiaomi_sm8250.git -b sched-4 kernel
